01cd7ac23e162e28ba434430b16b9083cea68e46
Evgenii Zakharchenko <evgenii.zakharchenko@jetbrains.com>
2025-06-25 18:16:27 +0200

    Create a Data Transfer Object (DTO) for the Developer entity. The DTO should include fields for id, name, and email address. Additionally, implement a mapper interface using MapStruct to convert between the Developer entity and the DeveloperDto. Ensure that the mapper is properly configured to be used as a Spring component.

---
+++ src/main/java/com/sivalabs/ft/features/domain/dtos/DeveloperDto.java
@@ -0,0 +1,9 @@
+package com.sivalabs.ft.features.domain.dtos;
+
+import java.io.Serializable;
+
+public record DeveloperDto(
+        Long id,
+        String name,
+        String emailAddress) implements Serializable {
+}
